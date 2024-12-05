d0314d135eb15f8a746aff340847d810afc27209
404boboo <100701180+404boboo@users.noreply.github.com>
2024-12-05 02:21:48 +0100

    Fix UART Communication Issue with TMC2209
    - Resolved an issue where UART communication with the TMC2209 was unreliable due to incorrect handling of the receive buffer.
    - Implemented logic to handle `HAL_UART_Receive_IT` correctly by ensuring it is called before transmitting data.
    - Added functionality to ignore the transmitted data that appears in the `rxData` buffer when performing a read operation immediately after a write.
    - Updated the library to include a note about this behavior

--- src/TMC2209.c
+++ src/TMC2209.c
@@ -1,6 +1,80 @@
+
+/******************************************************************************
+ * @file       TMC2209.cpp
+ * @author     Ahmed Bouras
+ * @date       05/12/2024
+ * @version    0.6
+ *
+ * @copyright  Copyright (c) [2024] Ahmed Bouras
+ *
+ * @license    Permission is hereby granted, free of charge, to any person
+ *             obtaining a copy of this software and associated documentation
+ *             files (the "Software"), to deal in the Software without
+ *             restriction, including without limitation the rights to use,
+ *             copy, modify, merge, publish, distribute, sublicense, and/or
+ *             sell copies of the Software, and to permit persons to whom
+ *             the Software is furnished to do so, subject to the following
+ *             conditions:
+ *
+ *             The above copyright notice and this permission notice shall
+ *             be included in all copies or substantial portions of the
+ *             Software.
+ *
+ *             THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+ *             KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ *             WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ *             PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ *             OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ *             OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ *             OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ *             SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * @note       This library provides an interface for controlling the TMC2209
+ *             stepper motor driver over STEP, DIR, ENN & UART. It is important
+ *             to note that receiving data from the TMC2209 is not possible
+ *             unless `HAL_UART_Receive_IT` is called before transmitting any
+ *             commands. Without this, the data transmitted will also appear
+ *             in the `rxData` buffer as part of the received data, which is
+ *             why this library includes logic to ignore those bytes. This
+ *             issue occurs specifically when reading directly after a write
+ *             command.
+ ******************************************************************************
+ */
+
 #include "TMC2209.h"
-#include "TMC2209_configs.h"
 #include "string.h"
+
+uint32_t last_tmc_read_attempt_ms = 0;
+uint8_t rxData[RX_REPLY_SIZE]; // received data on the RX line it is 12 in this case 4(transmitted buffer) + 8 (received buffer)
+uint8_t rxBuffer[TMC_REPLY_SIZE];
+volatile uint8_t dataReady = 0; // Flag to indicate data reception
+volatile uint8_t dataReadyFlag = 0;
+
+////////// HAL FUNCTIONS //////////
+
+// PWM callback for step counting
+void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef *htim)
+{
+  for(int i = 0; i < MAX_MOTORS; i++){
+	  if (htim->Instance == motors[i].driver.htim->Instance){ // Check which motor's timer called back
+		  motors[i].stepsTaken++;  // increment counter
+	  }
+  }
+
+}
+
+// UART callback for read from TMC2209
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
+    if (huart->Instance == USART2) {
+        for (uint8_t i = 0; i <TMC_REPLY_SIZE ; i++) {
+            rxBuffer[i] = rxData[i + TMC_READ_REQUEST_DATAGRAM_SIZE];
+
+        }
+        dataReadyFlag = 1;
+
+       HAL_UART_Receive_IT(&huart2, rxData, RX_REPLY_SIZE); // Re-enable UART receive
+    }
+}
 
 
 // Set the direction of the motor
@@ -48,24 +122,13 @@ void TMC2209_Start(Motor *motor) {
 	TIM_HandleTypeDef *htim = motor->driver.htim;
 	uint32_t channel = motor->driver.step_channel;
 
-	TMC2209_EnableDriver(motor, GPIO_PIN_RESET);
+	//TMC2209_EnableDriver(motor, GPIO_PIN_RESET);
     HAL_TIM_PWM_Start_IT(htim, channel);
     motor->isStepping = true;
 }
 
 
-void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef *htim)
-{
-  /* Prevent unused argument(s) compilation warning */
-  UNUSED(htim);
 
-  for(int i = 0; i < MAX_MOTORS; i++){
-	  if (htim->Instance == motors[i].driver.htim->Instance){ // Check which motor's timer called back
-		  motors[i].stepsTaken++;  // increment counter
-	  }
-  }
-
-}
 
 static void TMC2209_CountSteps(Motor *motor, uint32_t totalSteps){ // Static for now unless we need to expose it later
 	motor->nextTotalSteps = totalSteps;
@@ -88,83 +151,160 @@ void TMC2209_checkStatus(Motor *motor, bool *isStepping, uint32_t *nextTotalStep
      *nextTotalSteps = motor->nextTotalSteps;
 }
 
-// CRC calculation
-uint8_t calculate_crc(uint8_t *data, uint8_t length) {
+ void debug_print(const char* msg) {
+    HAL_UART_Transmit(&huart3, (uint8_t*)msg, strlen(msg), 100);
+}
+
+ void debug_print_hex(uint8_t* data, uint8_t length) {
+    char buffer[100];
+    char* ptr = buffer;
+
+    ptr += sprintf(ptr, "[");
+    for(uint8_t i = 0; i < length; i++) {
+        ptr += sprintf(ptr, "%02X ", data[i]);
+    }
+    ptr += sprintf(ptr, "]\r\n");
+
+    debug_print(buffer);
+}
+
+uint8_t calculate_CRC(uint8_t *datagram, uint8_t length) {
     uint8_t crc = 0;
-    for (uint8_t i = 0; i < length; i++) {
-        crc ^= data[i];
+    for(uint8_t i = 0; i < length; i++) {
+        uint8_t currentByte = datagram[i];
+        for(uint8_t j = 0; j < 8; j++) {
+            if((crc >> 7) ^ (currentByte & 0x01)) {
+                crc = (crc << 1) ^ 0x07;
+            } else {
+                crc = crc << 1;
+            }
+            currentByte >>= 1;
+        }
     }
-    return crc; // Return calculated CRC
+    return crc;
 }
 
-void TMC2209_SendCommand(Motor *motor, uint8_t reg_addr, uint32_t data) {
-    uint8_t command[8];
-    command[0] = 0x05; // Start byte
-    command[1] = motor->driver.address; // Driver address
-    command[2] = reg_addr; // Register address
-    command[3] = (data & 0xFF); // Send data (lower byte)
-    command[4] = (data >> 8) & 0xFF;
-    command[5] = (data >> 16) & 0xFF;
-    command[6] = (data >> 24) & 0xFF;
 
-    // Calculate CRC -- optional
-    command[7] = calculate_crc(command, 7);
+void clear_UART_buffers(UART_HandleTypeDef *huart) {
+    debug_print("Clearing UART buffers...\r\n");
 
-    // Transmit via UART
-    HAL_UART_Transmit(motor->driver.huart, command, 8, 10);
+    // Clear UART flags
+    __HAL_UART_CLEAR_PEFLAG(huart);
+    __HAL_UART_CLEAR_FEFLAG(huart);
+    __HAL_UART_CLEAR_NEFLAG(huart);
+    __HAL_UART_CLEAR_OREFLAG(huart);
+
+    // Flush receive buffer
+    uint8_t dummy;
+    while(__HAL_UART_GET_FLAG(huart, UART_FLAG_RXNE)) {
+        dummy = (uint8_t)(huart->Instance->RDR & 0x00FF);
+    }
+    (void)dummy;
 }
 
-// Read a register from the driver
-uint32_t TMC2209_ReadRegister(Motor *motor, uint8_t reg_addr) {
-    uint8_t command[7];
-    // Expected response size
-    uint8_t reply[8];
-    uint32_t data = 0;
-    // Prepare read command
-    command[0] = 0x05; // Start byte for TMC2209
-    command[1] = motor->driver.address; // Driver address
-    command[2] = reg_addr | 0x80; // Set the read bit
-    command[3] = 0; // Data not used for reading
-    command[4] = 0;
-    command[5] = 0;
 
-    // Calculate CRC  TODO: finish Checksum function -- DONE
-    command[6] = calculate_crc(command, 7);
 
-    // Send command
-    if(HAL_UART_Transmit(motor->driver.huart, command, 7, 10)  != HAL_OK){
-    return TMC2209_UART_ERROR_TX;
-    }
-    // Receive response
-    if(HAL_UART_Receive(motor->driver.huart, reply, 8, 10) != HAL_OK){
-    return TMC2209_UART_ERROR_RX;
-    }
 
-    /////// DEBUGGING: What the driver is sending back ///////
-    /*
-    printf("Received Reply: ");
-    for (int i = 0; i < sizeof(reply); i++) {
-        printf("0x%02X ", reply[i]); // Print each byte
-    }*/
 
-    ////////////////////////////////////////////////////////
 
-    // Validate reply
-    if (reply[0] != 0x05 || reply[1] != motor->driver.address || reply[2] != (reg_addr | 0x80)){
-        return TMC2209_INVALID_ADDRESS;
-    }
 
-       // Verify CRC
-    uint8_t received_crc = reply[7];
-   if (received_crc != calculate_crc(reply, 7)){
-   return TMC2209_UART_ERROR_CRC; // CRC error
-   }
+uint8_t TMC2209_WaitForReply(uint32_t timeout) {
+     uint32_t startTime = HAL_GetTick();
+     while (!dataReadyFlag) {
+         if ((HAL_GetTick() - startTime) > timeout) {
+             debug_print("Timeout waiting for reply.\r\n");
+             return 0; // Timeout
+         }
+     }
+     dataReadyFlag = 0; // Clear flag for next use
+     return 1; // Success
+ }
 
-       // Parse the 4-byte data payload from reply
-   data = (reply[3]) | (reply[4] << 8) | (reply[5] << 16) | (reply[6] << 24);
-        return data;
-}
 
+uint8_t* TMC2209_sendCommand(uint8_t *command, size_t writeLength, size_t readLength) {
+     // Making sure receive interrupt is disabled before sending to not fill rxBuffer with what is transmitted
+
+     // Send the command
+     if (HAL_UART_Transmit(&huart2, command, writeLength, 10) != HAL_OK) {
+         debug_print("Failed to send command.\r\n");
+         return 0;
+     }
+
+     debug_print("Data Transmitted: ");
+     debug_print_hex(command, writeLength);
+     clear_UART_buffers(&huart2);
+
+
+     if(readLength){
+     // Wait for reply
+     if (!TMC2209_WaitForReply(200)) { // Timeout after 200ms if no reply
+         debug_print("No reply received.\r\n");
+         return 0; // command failed
+     }
+
+     // Process received data in rxBuffer
+     debug_print("Reply received:\r\n");
+     debug_print_hex(rxBuffer, TMC_REPLY_SIZE);
+
+     return rxBuffer; // Success
+     }
+
+     return 1;
+ }
+
+
+
+ void TMC2209_writeInt(Motor *tmc2209, uint8_t regAddress, int32_t value){
+ 	uint8_t write_request_command[8];
+ 	write_request_command[0] = SYNC; // SYNC Byte for TMC2209
+ 	write_request_command[1] = tmc2209->driver.address; // Driver address configure it using MS1(LSB) AND MS2
+ 	write_request_command[2] = regAddress | 0x80; // Register address to write 0x80 for writing
+ 	write_request_command[3] = (value >> 24) & 0xFF;
+ 	write_request_command[4] = (value >> 16) & 0xFF;
+ 	write_request_command[5] = (value >> 8 ) & 0xFF;
+ 	write_request_command[6] = (value      ) & 0xFF;
+ 	write_request_command[7] = calculate_CRC(write_request_command, 7); // checksum
+ 	TMC2209_sendCommand(&write_request_command[0], TMC_WRITE_DATAGRAM_SIZE, 0); // We don't actually need receive buffer here when we call ReadWrite so we just pass data
+
+ }
+
+int32_t TMC2209_readInt(Motor *tmc2209, uint8_t regAddress){
+ 	uint8_t read_request_command[8] = { 0 };
+
+// 	if (!TMC_IS_READABLE(tmc2209->registerAccess[address]))
+// 		return tmc2209->config->shadowRegister[address];
+
+ 	read_request_command[0] = SYNC;
+ 	read_request_command[1] = tmc2209->driver.address;
+ 	read_request_command[2] = regAddress;
+ 	read_request_command[3] = calculate_CRC(read_request_command, 3);
+
+ 	uint8_t *verifyBuffer = TMC2209_sendCommand(read_request_command, TMC_READ_REQUEST_DATAGRAM_SIZE, TMC_REPLY_SIZE);
+ 	// Byte 0: Sync nibble correct?
+ 	if (verifyBuffer[0] != 0x05){
+ 		// If first byte equals 0 then it means no reply so return
+ 		if (verifyBuffer[0] == 0)
+ 			return 0;
+ 		debug_print("Invalid data received!(SYNC Byte)\r\n");
+ 		return 0;
+ 	}
+ 	// Byte 1: Master address correct?
+ 	if (verifyBuffer[1] != 0xFF){
+ 		debug_print("Invalid data received!(MCU Address)\r\n");
+ 		return 0;
+ 	}
+ 	// Byte 2: Register ddress correct?
+ 	if (verifyBuffer[2] != regAddress){
+ 		debug_print("Invalid data received!(Register Address)\r\n");
+ 		return 0;
+ 	}
+ 	// Byte 7: CRC correct?
+ 	if (verifyBuffer[7] != calculate_CRC(verifyBuffer, 7)){
+ 		debug_print("Invalid data received!(CRC)\r\n");
+ 		return 0;
+ 	}
+ 	return (verifyBuffer[3] << 24) | (verifyBuffer[4] << 16) | (verifyBuffer[5] << 8) | verifyBuffer[6];
+ }
 
 
 
